3683aff00ccd87af276ef29a1da137769f9f3f93
tofu4956 <dfected999ethnotverified@protonmail.com>
2022-04-07 07:12:25 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Script defer strategy="beforeInteractive" data-domain="tofu4956.net" src="https://internal.tofu4956.net/js/plausible.js" />
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
