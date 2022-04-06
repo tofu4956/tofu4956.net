cae43c073798f254c7423b94c29916a69459d9e5
tofu4956 <dfected999ethnotverified@protonmail.com>
2022-04-07 04:52:04 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      In construction
+      <a style={{}}href="http://blog.tofu4956.net" rel="noopener noreferrer">ブログ</a>
+      <a href="http://twitter.com/tofu4956" rel="noopener noreferrer">Twitter</a>
+    </div>
+  )
+}
+
+export default Home
